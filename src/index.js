f72695df43bbf09ef39e333e268f233cd38c857f
Michał Budzyński <michal@virtualdesign.pl>
2018-08-25 16:27:00 +0100

    :school_satchel: render floor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Game } from 'cervus/core';
 import { Box, Plane } from 'cervus/shapes';
 import { PhongMaterial } from 'cervus/materials';
 import { Render, Transform, Move } from 'cervus/components';
+import map from './map.json';
 
 export const game = new Game({
     width: window.innerWidth,
@@ -11,15 +12,27 @@ export const game = new Game({
 
 
 const camera_transform = game.camera.get_component(Transform);
-camera_transform.position = [0.074, 10.629, -8.845]
-camera_transform.rotation = [0.387, 0, 0, 0.921];
+camera_transform.position = [map.starting_point.x, 0.42, map.starting_point.y]
+camera_transform.rotation = [-0.067, 0.00169, 0.0001139, 0.9977];
 window.camera = camera_transform;
 game.camera.get_component(Move).keyboard_controlled = true;
-game.camera.get_component(Move).mouse_controlled = true;
+// game.camera.get_component(Move).mouse_controlled = true;
 
 const material = new PhongMaterial({
     requires: [Render, Transform]
 });
+
+
+
+const plane = new Plane();
+const plane_transform = plane.get_component(Transform);
+const plane_render = plane.get_component(Render);
+plane_transform.scale = [map.size.x, 1, map.size.y];
+plane_transform.position = [0.5, -0.5, 0.5];
+plane_render.material = material;
+plane_render.color = "#ff00ff";
+game.add(plane);
+
 
 const box = new Box();
 box.get_component(Transform).position = [0, 0, 3];
@@ -27,12 +40,3 @@ window.box_transform = box.get_component(Transform);
 box.get_component(Render).material = material;
 box.get_component(Render).color = '0F0';
 game.add(box);
-
-const plane = new Plane();
-const plane_transform = plane.get_component(Transform);
-const plane_render = plane.get_component(Render);
-plane_transform.scale = [10, 1, 10];
-plane_transform.position = [0.5, -0.5, 0.5];
-plane_render.material = material;
-plane_render.color = "#ff00ff";
-game.add(plane);
