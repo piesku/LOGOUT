5edd52cfa545153d25c9382491005f103664452c
Staś Małolepszy <stas@duzodobrze.pl>
2018-08-30 02:24:48 +0200

    Remove livereload from Cervus dist

--- public/cervus.js
+++ public/cervus.js
@@ -1,4 +1,3 @@
-document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
 var Cervus = (function (exports) {
 'use strict';
 
