5955520ebd7af5e3b84e313a88ca1435a73f8b48
Staś Małolepszy <stas@duzodobrze.pl>
2018-08-30 18:46:28 +0200

    Game.track_entity

--- public/cervus.js
+++ public/cervus.js
@@ -2826,9 +2826,8 @@ class Entity {
     this.entities.add(entity);
 
     if (this.game) {
-      // If this entity already exists, make sure its children's components are
-      // accounted for.
-      this.game.add_to_components_sets(entity);
+      // Track the child if the current entity already exists in the scene.
+      this.game.track_entity(entity);
     }
   }
 
@@ -3591,7 +3590,9 @@ class Game {
     this.emit('afterrender');
   }
 
-  add_to_components_sets(entity) {
+  track_entity(entity) {
+    entity.game = this;
+
     for (let component of entity.components.values()) {
       if (!this.entities_by_component.has(component.constructor)) {
         this.entities_by_component.set(component.constructor, new Set());
@@ -3599,18 +3600,20 @@ class Game {
       this.entities_by_component.get(component.constructor).add(entity);
     }
 
-    // Recursively add children's components, too.
+    // Recursively track children.
     for (let child of entity.entities) {
-      this.add_to_components_sets(child);
+      this.track_entity(child);
     }
   }
 
-  remove_from_components_sets(entity) {
+  untrack_entity(entity) {
+    entity.game = null;
+
     for (let component of entity.components.values()) {
       this.entities_by_component.get(component.constructor).delete(entity);
     }
 
-    // Recursively remove children's components, too.
+    // Recursively untrack children.
     for (let child of entity.entities) {
       this.remove_from_components_sets(child);
     }
@@ -3621,14 +3624,13 @@ class Game {
   }
 
   add(entity) {
-    entity.game = this;
     this.entities.add(entity);
-    this.add_to_components_sets(entity);
+    this.track_entity(entity);
   }
 
   remove(entity) {
-    this.remove_from_components_sets(entity);
     this.entities.delete(entity);
+    this.untrack_entity(entity);
   }
 }
 
