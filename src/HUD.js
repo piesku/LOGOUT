f35442f856c3d2352a19ca933b8d3c9b372ef5fb
Stas Malolepszy <stas@piesku.com>
2018-09-05 22:06:07 +0700

    Move HUD logic to lifecycle methods

--- src/HUD.js
+++ src/HUD.js
@@ -8,6 +8,60 @@ import game from "./game";
 
 function HUD({lastActive, systems}) {
     return new class extends Component {
+        constructor() {
+            super();
+            this.nf = new Intl.NumberFormat("en", {
+                minimumFractionDigits: 3,
+                maximumFractionDigits: 3
+            });
+        }
+
+        before(root) {
+            clearInterval(this.interval);
+        }
+
+        after(root) {
+            this.interval = setInterval(() => {
+                let div = document.createElement("div");
+
+                // Animate code display
+                let br = root.querySelector(".br");
+                br.removeChild(br.firstElementChild);
+                div.innerHTML = Line(code_anim.next().value);
+                br.appendChild(div.firstElementChild);
+
+                // Update datetime
+                let tr = root.querySelector(".tr .glitch");
+                for (let child of tr.children) {
+                    child.textContent = (new Date()).toString();
+                }
+            }, 1000);
+
+            game.on("afterrender", () => {
+                // Update local matrix display
+                let bl = root.querySelector(".bl");
+                let matrix = game.camera.get_component(Cervus.components.Transform).matrix;
+                let values = [
+                    [matrix[0], matrix[4], matrix[8], matrix[12]],
+                    [matrix[1], matrix[5], matrix[9], matrix[13]],
+                    [matrix[2], matrix[6], matrix[10], matrix[14]],
+                    [matrix[3], matrix[7], matrix[11], matrix[15]]
+                ];
+
+                // Skip the header.
+                let children = [...bl.children].slice(1);
+                for (let [i, line] of children.entries()) {
+                    for (let child of line.querySelector(".glitch").children) {
+                        child.textContent = values[i].map(
+                            n => this.nf.format(n)
+                        ).join(" ");
+                    }
+
+                    i++;
+                }
+            });
+        }
+
         render() {
             let systems_status = [];
             for (let [sys, stat] of Object.entries(systems)) {
@@ -53,51 +107,3 @@ function HUD({lastActive, systems}) {
 }
 
 export default connect(HUD);
-
-let root = document.querySelector("#root");
-
-let interval;
-root.addEventListener("beforerender", function() {
-    clearInterval(interval);
-});
-root.addEventListener("afterrender", function() {
-    interval = setInterval(() => {
-        let div = document.createElement("div");
-
-        // Animate code display
-        let br = root.querySelector(".br");
-        br.removeChild(br.firstElementChild);
-        div.innerHTML = Line(code_anim.next().value);
-        br.appendChild(div.firstElementChild);
-
-        // Update datetime
-        let tr = root.querySelector(".tr .glitch");
-        for (let child of tr.children) {
-            child.textContent = (new Date()).toString();
-        }
-    }, 1000);
-});
-
-let nf = new Intl.NumberFormat("en", {minimumFractionDigits: 3, maximumFractionDigits: 3});
-
-game.on("afterrender", function() {
-    // Update local matrix display
-    let bl = root.querySelector(".bl");
-    let matrix = game.camera.get_component(Cervus.components.Transform).matrix;
-    let values = [
-        [matrix[0], matrix[4], matrix[8], matrix[12]],
-        [matrix[1], matrix[5], matrix[9], matrix[13]],
-        [matrix[2], matrix[6], matrix[10], matrix[14]],
-        [matrix[3], matrix[7], matrix[11], matrix[15]]
-    ];
-
-    // Skip the header.
-    let children = [...bl.children].slice(1);
-    for (let [i, line] of children.entries()) {
-        for (let child of line.querySelector(".glitch").children) {
-            child.textContent = values[i].map(n => nf.format(n)).join(" ");
-        }
-
-        i++;
-    }
-});
