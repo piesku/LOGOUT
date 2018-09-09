5bd8e3fd6be26175485b5c39b4ae10d977f0252f
Stas Malolepszy <stas@piesku.com>
2018-09-09 23:32:02 +0700

    Rephrase intro copy

--- src/Intro.js
+++ src/Intro.js
@@ -10,18 +10,16 @@ const text = `
 <div class="big">LOGOUT</div>
 
 In 2018, people live their lives in virtual reality.
-Most spent their entire lives in community clusters
-assigned to them at birth. More lucky ones migrate
-to premium clusters which boast 99,999% uptime.
+Most never leave the community clusters assigned
+to them at birth. A handful of lucky ones migrate
+to premium clusters which boast 100% uptime.
 
 You are not one of the lucky ones.
 
 The community VR cluster you've spent your life in
 has had a critical failure. The reboot has reset
-the entire world.
-
-Most systems are still offline but there should be
-a way to bring them up online.
+the entire world. Most systems are still offline
+but there should be a way to bring them up online.
 
 Find the exit and log out into the reality.
 
