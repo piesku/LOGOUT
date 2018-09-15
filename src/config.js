846d3c02ad218b4cda05c1a5977e9353fe8580dc
Stas Malolepszy <stas@piesku.com>
2018-09-15 21:40:28 +0700

    Reduce powerup light intensity

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const NEON_LIGHT_MOUNT = [0, 0, -20];
 export const NEON_INTENSITY = 15;
 
 export const POWERUP_COLOR = "fff";
-export const POWERUP_INTENSITY = 15;
+export const POWERUP_INTENSITY = 5;
 
 export const MAX_BUILDING_HEIGHT = 32;
 export const MIN_BUILDING_HEIGHT = 8;
