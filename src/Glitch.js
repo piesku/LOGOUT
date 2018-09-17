50c4dddb383f4e7159c44f638755649fb2d7843b
Staś Małolepszy <stas@duzodobrze.pl>
2018-09-18 00:31:01 +0200

    Shorter glitch class names

--- src/Glitch.js
+++ src/Glitch.js
@@ -5,18 +5,18 @@ export default
 function Glitch(text) {
     // A deterministic digest function provides variety and ensures
     // subsequent renders given the same state produce the same output.
-    let glitch_index = digest(text) % 20;
+    let i = digest(text) % 20;
     return html`
-        <div class="glitch">
-            <div class="glitch1 noise${glitch_index}">${text}</div>
-            <div class="glitch2 noise${glitch_index}">${text}</div>
-            <div class="glitch3 noise${glitch_index}">${text}</div>
+        <div class="g">
+            <div class="g1 n${i}">${text}</div>
+            <div class="g2 n${i}">${text}</div>
+            <div class="g3 n${i}">${text}</div>
         </div>
     `;
 }
 
 export function set_glitch(line, text) {
-    for (let glitchPart of line.querySelectorAll(".glitch > div")) {
+    for (let glitchPart of line.querySelectorAll(".g > div")) {
         glitchPart.textContent = text;
     }
 }
