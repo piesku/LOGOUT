6ebcff00cfca31f4a52d6006787e6cbd55494542
Staś Małolepszy <stas@duzodobrze.pl>
2018-09-17 14:09:49 +0200

    Add innerself logger

--- src/store.js
+++ src/store.js
@@ -1,4 +1,6 @@
 import createStore from "./innerself";
+import with_logger from "./innerself/logger";
 import reducer from "./reducer";
 
-export const {attach, connect, dispatch, html} = createStore(reducer);
+export const {attach, connect, dispatch, html} =
+    createStore(with_logger(reducer));
