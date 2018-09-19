dcbd853475e0f31af1b930b5d8bd341f07cf8c2e
Staś Małolepszy <stas@duzodobrze.pl>
2018-09-19 12:50:01 +0200

    Leaner Matrix.js

--- src/Matrix.js
+++ src/Matrix.js
@@ -5,43 +5,28 @@ import {Transform} from './cervus/components';
 
 export default function Matrix(cls, styles) {
     return new class extends Component {
-        constructor() {
-            super();
-            this.nf = new Intl.NumberFormat("en", {
-                minimumFractionDigits: 3,
-                maximumFractionDigits: 3
-            });
-        }
-
         before() {
             game.off("afterrender", this.up);
         }
 
         after(root) {
+            // Skip the header.
             let rows = Array.from(
                 root.querySelectorAll(`.${cls} .l`)).slice(1);
             this.up = game.on("afterrender", () => {
                 // Update local matrix display
                 let matrix = game.camera.components.get(Transform).matrix;
-                let values = [
-                    [matrix[0], matrix[4], matrix[8], matrix[12]],
-                    [matrix[1], matrix[5], matrix[9], matrix[13]],
-                    [matrix[2], matrix[6], matrix[10], matrix[14]],
-                    [matrix[3], matrix[7], matrix[11], matrix[15]]
-                ];
-
-                // Skip the header.
                 for (let [i, line] of rows.entries()) {
                     set_glitch(
                         line,
-                        values[i].map(n => this.nf.format(n)).join(" "));
+                        Array.from(matrix.slice(4 * i, 4 * i + 4), n => n.toFixed(3)).join(" "));
                 }
             });
         }
 
         render() {
             return Block(cls, [
-                "Avatar entity matrix", "1", "2", "3", "4"
+                "Entity matrix", "1", "2", "3", "4"
             ], styles);
         }
     }
